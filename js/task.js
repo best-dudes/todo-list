cd76079e18dc7696e3fa0359e717b4c259c816c4
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2018-12-24 16:57:23 +0200

    Dudes (#5)
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Update task.js
    
    * Update task.js
    
    * Update task.js
    
    * Update index.html
    
    * Update task.js

--- js/task.js
+++ js/task.js
@@ -1,7 +1,7 @@
-var textEl = document.getElementById('text');
-var buttonEl = document.getElementById('button');
-
-function getVal() {
-  var text = textEl.value;
-  console.log(text);
+function onClick() {
+  var text = document.getElementById('text').value;
+  var paragraph = document.createElement('p');
+  var t = document.createTextNode(text);
+  paragraph.appendChild(t);
+  document.getElementById('wrapper').appendChild(paragraph);
 }
