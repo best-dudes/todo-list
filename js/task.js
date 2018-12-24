6e725177cefa6e69eae052c06fed926b9ebc2a77
Dmytro Mykhalenych <41123786+dm-mykhalenych@users.noreply.github.com>
2018-12-24 14:56:32 +0200

    Update task.js

--- js/task.js
+++ js/task.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var text = document.getElementById('text').value;
-  var paragraf = document.createElement("p");
+  var paragraph = document.createElement("p");
   var t = document.createTextNode(text);
   paragraph.appendChild(t);
   document.getElementById("wrapper").appendChild(paragraph);
